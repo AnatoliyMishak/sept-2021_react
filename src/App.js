f890d1f9ab69c0c4d54114a4ef8a5dc7c548a887
AnatoliyMishak <mishak_tolja@ukr.net>
2022-01-13 23:10:45 +0200

    Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Notebook from './Components/Notebook';
 
 const App = () => {
     return (
@@ -12,7 +13,8 @@ const App = () => {
             {/*        alt={'Apple'}/>*/}
             {/*</div>*/}
 
-            <Notebook/>
+            <Notebook brand={'Apple'} Model={'Pro'} Cost={100} img={'https://www.apple.com/v/macbook-pro-14-and-16/a/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png?202111300609'}/>
+            <Notebook brand={'HP'} Model={'15S'} Cost={1000} img={'https://www.notebookcheck-ru.com/fileadmin/Notebooks/HP/15s-fq1556ng/HP_15s_fq_teaser.png'}/>
 
 
             {/*<div>*/}
--- src/Components/Notebook.js
+++ src/Components/Notebook.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Notebook = () => {
+const Notebook = (props) => {
+    console.log(props);
     return (
         <div>
                 <h1>Brand: Apple</h1>
