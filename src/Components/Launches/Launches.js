8bf3ce6292b9deefb5f38f395bffbfea59233b0b
AnatoliyMishak <mishak_tolja@ukr.net>
2022-01-15 00:07:55 +0200

    CW1

---
+++ src/Components/Launches/Launches.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+import Launch from '../Launch/Launch'
+
+const Launches = () => {
+    const [launches, setLaunches] = useState([]);
+
+    useEffect(() =>{
+        fetch('https://api.spacexdata.com/v3/launches')
+            .then(value => value.json())
+            .then(launches => {
+                setLaunches(launches.filter(launch => launch.launch_year !=='2020'));
+            });
+    },[])
+    return (
+        <div>
+            {launches.map(value => <Launch key={value.flight_number}
+                                           mission_name={value.mission_name}
+                                           launch_year={value.launch_year}/>)}
+        </div>
+    );
+};
+
+export default Launches;
