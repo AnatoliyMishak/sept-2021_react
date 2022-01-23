1ec050bd100715f9e07908204ef6e3d16f376ede
AnatoliyMishak <mishak_tolja@ukr.net>
2022-01-23 20:28:30 +0200

    Lesson3_trial_1_2_postSearch

--- src/Pages/PostsPage/PostsPage.js
+++ src/Pages/PostsPage/PostsPage.js
@@ -1,18 +1,35 @@
 import React, {useEffect, useState} from 'react';
 import {postService} from "../../Services/Post.service";
 import Post from "../../Components/Post/Post";
-import {Outlet} from "react-router-dom";
+import {Outlet, useSearchParams} from "react-router-dom";
 import css from './PostsPage.module.css'
 
 const PostsPage = () => {
     const [posts, setPosts] = useState([]);
+    const [query, setQuery] = useSearchParams();
 
     useEffect(() => {
-        postService.getAll().then(value => setPosts([...value]))
-    }, [])
+        postService.getAll().then(value => {
+            const title = query.get('title');
+            let filter = [...value]
+            if(title){
+               filter = value.filter(post => post.title.includes(title))
+            }
+            setPosts(filter)
+        })
+    }, [query])
+
+    const submit = (e) => {
+      e.preventDefault()
+        setQuery({title:e.target.search.value})
+    }
 
     return (
         <div>
+            <form onSubmit={submit}>
+                <input type="search" name={'search'}/>
+                <button>Search</button>
+            </form>
             <h1>Posts</h1>
             <div className={css.target}>
                 <div className={css.post}>
