2f2d047c135233ad86e97a2eecccc0a91e9234b2
AnatoliyMishak <mishak_tolja@ukr.net>
2022-01-25 20:53:47 +0200

    Hw_3

--- src/Pages/PostsPage/PostsPage.js
+++ src/Pages/PostsPage/PostsPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {postService} from "../../Services/Post.service";
 import Post from "../../Components/Post/Post";
+import {Outlet} from "react-router-dom";
 
 const PostsPage = () => {
     const [posts, setPosts] = useState([]);
@@ -13,6 +14,7 @@ const PostsPage = () => {
         <div>
             <h1>Posts</h1>
             {posts.map(post => <Post key={post.id} post={post}/>)}
+            <Outlet/>
         </div>
     );
 };
--- src/Pages/UsersPage/UsersPage.js
+++ src/Pages/UsersPage/UsersPage.js
@@ -1,18 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import {userService} from "../../Services/User.service";
 import User from "../../Components/User/User";
+import {Outlet} from "react-router-dom";
+import css from './UsersPage.module.css'
 
 const UsersPage = () => {
     const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         userService.getAll().then(value => setUsers([...value]))
-    },[])
+    }, [])
 
     return (
-        <div>
-            <h1>Users</h1>
-            {users.map(user=><User key={user.id} user={user}/>)}
+        <div className={css.userpage}>
+            <div>
+                <h1>Users</h1>
+                {users.map(user => <User key={user.id} user={user}/>)}
+            </div>
+            <div>
+                <Outlet/>
+            </div>
         </div>
     );
 };
