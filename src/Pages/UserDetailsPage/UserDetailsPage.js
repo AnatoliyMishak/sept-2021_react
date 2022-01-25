6fd2f411655bad7e9b02c4793509e7f7671777b7
AnatoliyMishak <mishak_tolja@ukr.net>
2022-01-25 21:13:21 +0200

    Hw_3

--- src/Pages/UserDetailsPage/UserDetailsPage.js
+++ src/Pages/UserDetailsPage/UserDetailsPage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {userService} from "../../Services/User.service";
 
 const UserDetailsPage = () => {
+    const {id} = useParams();
+    const [user, setUser] = useState(null);
+
+    useEffect(()=>{
+        userService.getById(id).then(value => setUser({...value}))
+    },[])
     return (
         <div>
-            UserDetails
+            {user && (
+                <div>{user.id}</div>
+                // <div>{user.name}</div>
+                // <div>{user.username}</div>
+                // <div>{user.email}</div>
+            )}
         </div>
     );
 };
