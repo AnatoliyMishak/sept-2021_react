b6bb3b4edd1e09e4d4b265ff8cd8a59294a0dcf5
AnatoliyMishak <mishak_tolja@ukr.net>
2022-01-25 20:59:23 +0200

    Hw_3

--- src/Services/User.service.js
+++ src/Services/User.service.js
@@ -2,5 +2,6 @@ import {axiosService} from "./Axios.service";
 import {urls} from "../Configs/Urls";
 
 export const userService={
-    getAll:()=>axiosService.get(urls.users).then(value => value.data)
+    getAll:()=>axiosService.get(urls.users).then(value => value.data),
+    getById:(id)=>axiosService.get(`${urls.users}/${id}`).then(value => value.data)
 }
