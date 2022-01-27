a8f5c9b205de2ed5f155579d3b2f0b4b5d81960b
AnatoliyMishak <mishak_tolja@ukr.net>
2022-01-27 19:57:49 +0200

    Hw_3

--- src/Pages/PostDetailsPage/PostDetailsPage.js
+++ src/Pages/PostDetailsPage/PostDetailsPage.js
@@ -1,28 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import {Outlet, useParams} from "react-router-dom";
+import {Link, Outlet, useParams} from "react-router-dom";
 import {postService} from "../../Services/Post.service";
 
 const PostDetailsPage = () => {
     const {id} = useParams();
 
     const [post, setPost] = useState(null);
-    const [comments, setComments] = useState([]);
 
     useEffect(()=>{
         postService.getById(id).then(value => setPost({...value}))
     },[id])
 
-    const getCommentsId = (id) => {
-      postService.getByComments(id).then(value => setComments([...value]))
-        setComments(comments)
-    }
 
     return (
         <div>
             {post && (
                 <div>{post.body}</div>
             )}
-            <button onClick={()=>getCommentsId(id)}>Comments</button>
+            <Link to={id.toString()} state={'comments'}>{<button>Comments</button>}</Link>
             <Outlet/>
         </div>
     );
